55f4829f158540567bbc4529ccd3c069c576383b
tubalmartin <tubalmartin@gmail.com>
2012-04-07 22:27:08 +0200

    Initial release!

---
+++ README.md
@@ -0,0 +1,28 @@
+# onDomReady
+
+A function that allows you to safely initialize dependent code of DOM ready state.
+
+* Bullet proof: Based on jQuery 1.7 ready() method (same behavior & reliability).
+* AMD compatible (anonymous module = more portable).
+
+## How to use:
+
+```js
+// Functions passed to onDomReady will be executed in order as soon as the DOM is ready.
+
+// Execute this function ASAP
+onDomReady( function() {
+    // Your code
+} );
+
+// Define a callback
+var init = function() {
+    // Your code
+};
+
+// Execute the init function ASAP
+onDomReady( init );
+
+// Get the version of onDomReady (string)
+var version = onDomReady.version; // "1.0"
+```
---
+++ ondomready.js
@@ -0,0 +1,130 @@
+!function (definition) {
+    if (typeof define === 'function' && define.amd) {
+        // Register as an AMD module.
+        define(definition);
+    } else {
+        // Browser globals
+        window.onDomReady = definition();
+    }
+}(function() {
+    var onreadystatechange = 'onreadystatechange',
+    DOMContentLoaded = 'DOMContentLoaded',
+    addEventListener = 'addEventListener',
+    attachEvent = 'attachEvent',
+    readyState = 'readyState',
+    complete = 'complete',
+    bfalse = false,
+    win = window,
+    doc = win.document,
+    docElem = doc.documentElement,
+    toplevel = bfalse,
+    
+    // Callbacks pending execution until DOM is ready
+    callbacks = [],
+    
+    // Is the DOM ready to be used? Set to true once it occurs.
+    isReady = bfalse,
+    
+    // The document ready event handler
+    DOMContentLoadedHandler;
+    
+    // Handle when the DOM is ready
+    function ready( fn ) {
+        if ( isReady ) {
+            return;
+        }
+        
+        // Make sure body exists, at least, in case IE gets a little overzealous.
+        if ( !doc.body ) {
+            return defer( ready );
+        }
+        
+        // Remember that the DOM is ready
+        isReady = true;
+
+        // Execute all callbacks
+        while ( fn = callbacks.shift() ) {
+            defer( fn );
+        }    
+    }
+    
+    // The DOM ready check for Internet Explorer
+    function doScrollCheck() {
+        if ( isReady ) {
+            return;
+        }
+    
+        try {
+            // If IE is used, use the trick by Diego Perini
+            // http://javascript.nwbox.com/IEContentLoaded/
+            docElem.doScroll('left');
+        } catch(e) {
+            return defer( doScrollCheck );
+        }
+    
+        // and execute any waiting functions
+        ready();
+    }
+    
+    // Defers a function, scheduling it to run after the current call stack has cleared.
+    function defer( fn ) {
+        win.setTimeout( fn, 1 );
+    }
+    
+    // Attach the listeners:
+    // Catch cases where onDomReady is called after the
+    // browser event has already occurred.
+    if ( doc[readyState] === complete ) {
+        ready();
+    } else {
+        // W3C event model
+        if ( doc[addEventListener] ) {
+            DOMContentLoadedHandler = function() {
+                doc.removeEventListener( DOMContentLoaded, DOMContentLoadedHandler, bfalse );
+                ready();
+            };
+            
+            // Use the handy event callback
+            doc[addEventListener]( DOMContentLoaded, DOMContentLoadedHandler, bfalse );
+    
+            // A fallback to window.onload, that will always work
+            win[addEventListener]( 'load', ready, bfalse );
+    
+        // IE event model
+        } else if ( doc[attachEvent] ) {
+            DOMContentLoadedHandler = function() {
+                if ( doc[readyState] === complete ) {
+                    doc.detachEvent( onreadystatechange, DOMContentLoadedHandler );
+                    ready();
+                }
+            };
+            
+            // ensure firing before onload,
+            // maybe late but safe also for iframes
+            doc[attachEvent]( onreadystatechange, DOMContentLoadedHandler );
+    
+            // A fallback to window.onload, that will always work
+            win[attachEvent]( 'onload', ready );
+    
+            // If IE and not a frame
+            // continually check to see if the document is ready
+            try {
+                toplevel = win.frameElement == null;
+            } catch(e) {}
+    
+            if ( docElem.doScroll && toplevel ) {
+                doScrollCheck();
+            }
+        }
+    } 
+    
+    function onDomReady( fn ) { 
+        // If DOM is ready, execute the function (async), otherwise wait
+        isReady ? defer( fn ) : callbacks.push( fn );
+    }
+    
+    // Add version
+    onDomReady.version = '1.0';
+    
+    return onDomReady;
+});
